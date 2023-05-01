cdf26ce57e25ca1934f33a90677abcfae5f70caf
Ali69 <130227966+Alighasemi98@users.noreply.github.com>
2023-05-01 20:31:06 +0430

    Guess-Number-Game

--- Game/Form1.Designer.cs
+++ Game/Form1.Designer.cs
@@ -34,29 +34,34 @@ namespace Game
             this.button1 = new System.Windows.Forms.Button();
             this.label2 = new System.Windows.Forms.Label();
             this.listBox1 = new System.Windows.Forms.ListBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(813, 30);
+            this.label1.Location = new System.Drawing.Point(250, 36);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(70, 25);
+            this.label1.Size = new System.Drawing.Size(35, 13);
             this.label1.TabIndex = 0;
             this.label1.Text = "label1";
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(243, 259);
+            this.textBox1.Location = new System.Drawing.Point(11, 33);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(311, 31);
+            this.textBox1.Size = new System.Drawing.Size(178, 20);
             this.textBox1.TabIndex = 1;
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(296, 306);
+            this.button1.Location = new System.Drawing.Point(11, 61);
+            this.button1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(192, 126);
+            this.button1.Size = new System.Drawing.Size(116, 56);
             this.button1.TabIndex = 2;
             this.button1.Text = "Check";
             this.button1.UseVisualStyleBackColor = true;
@@ -65,32 +70,56 @@ namespace Game
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(145, 107);
+            this.label2.Location = new System.Drawing.Point(193, 36);
+            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(70, 25);
+            this.label2.Size = new System.Drawing.Size(35, 13);
             this.label2.TabIndex = 3;
             this.label2.Text = "label2";
             // 
             // listBox1
             // 
             this.listBox1.FormattingEnabled = true;
-            this.listBox1.ItemHeight = 25;
-            this.listBox1.Location = new System.Drawing.Point(658, 118);
+            this.listBox1.Location = new System.Drawing.Point(147, 61);
+            this.listBox1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.listBox1.Name = "listBox1";
-            this.listBox1.Size = new System.Drawing.Size(295, 379);
+            this.listBox1.Size = new System.Drawing.Size(150, 173);
             this.listBox1.TabIndex = 4;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(11, 132);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(116, 40);
+            this.button2.TabIndex = 5;
+            this.button2.Text = "Auxiliary";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(11, 190);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(116, 33);
+            this.button3.TabIndex = 6;
+            this.button3.Text = "Answer";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
             // 
             // Form1
             // 
             this.AcceptButton = this.button1;
-            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(979, 500);
+            this.ClientSize = new System.Drawing.Size(315, 261);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
             this.Controls.Add(this.listBox1);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.textBox1);
             this.Controls.Add(this.label1);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Name = "Form1";
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
@@ -106,6 +135,8 @@ namespace Game
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
     }
 }
 
--- Game/Form1.cs
+++ Game/Form1.cs
@@ -12,20 +12,19 @@ namespace Game
 {
     public partial class Form1 : Form
     {
-        int rnd = 0, counter = 0;
+        int rnd = 0, counter = 0, minimum = 100, maximum = 999;
         public Form1()
         {
             InitializeComponent();
         }
-
-        private void button1_Click(object sender, EventArgs e)
+         void cod()
         {
             try
             {
 
                 int input = int.Parse(textBox1.Text);
                 textBox1.Text = "";
-                if (input > 999 || input < 10)
+                if (input > 999 || input < 100)
                 {
                     MessageBox.Show("Number is not in range");
                     return;
@@ -36,6 +35,7 @@ namespace Game
                 {
                     MessageBox.Show("Game Over!");
                     button1.Enabled = false;
+                    BackColor = Color.Red;
                     return;
                 }
                 listBox1.Items.Add(input);
@@ -48,6 +48,7 @@ namespace Game
                     MessageBox.Show("WIN");
                     label2.Text += " - You WIN";
                     button1.Enabled = false;
+                    BackColor = Color.Green;
                 }
             }
             catch (Exception ex)
@@ -56,12 +57,44 @@ namespace Game
             }
         }
 
+        private void button3_Click(object sender, EventArgs e)
+        {
+            int y = rnd;
+            textBox1.Text = y.ToString();
+            label1.Show();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            cod();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            int x = (maximum + minimum) / 2;
+            if (x > rnd)
+            {
+                maximum = x;
+                x = (maximum + minimum) / 2;
+            }
+            else
+            {
+                minimum = x;
+                x = (maximum + minimum) / 2;
+            }
+
+            textBox1.Text = x.ToString();
+            cod();
+
+        }
+
         private void Form1_Load(object sender, EventArgs e)
         {
             Random r = new Random();
             rnd = r.Next(100,999);
             label1.Text = rnd.ToString();
             label2.Text = "Round: " + counter.ToString();
+            label1.Hide();
         }
     }
 }
